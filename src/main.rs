07e4e968ca352526c1a4fbbb6fe036db4296969f
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2019-05-20 00:34:33 -0400

    segfault

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
+#[macro_use(array)]
 extern crate ndarray;
 extern crate ndarray_linalg;
-// extern crate blas;
-// extern crate netlib_src;
-extern crate openblas_src;
-// extern crate intel_mkl_src;
+extern crate intel_mkl_src;
 
 use ndarray::{Array,Ix2};
-use ndarray_linalg::{Factorize,Inverse};
+use ndarray_linalg::{Factorize,Inverse,SVD};
 
 fn main() {
 
     let mut mtx: Array<f64,Ix2> = Array::from_shape_vec((2,2),vec![1.,2.,3.,4.]).unwrap();
     mtx.factorize().unwrap().inv();
+
+    mtx = array![[1.,0.],[0.,1.]];
+    mtx.svd(true,true);
 
     println!("Hello, world!");
 
